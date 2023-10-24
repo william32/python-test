7d007d185c81bb778167f2fa16600e2abbe510e6
GGaubert <ggaubert@13.cl>
2023-10-24 16:15:08 -0300

    test first commit

---
+++ src/backup.py
@@ -0,0 +1,4 @@
+import sys
+print(sys.argv)
+print(sys.argv[0]) # program name
+print(sys.argv[1]) # first arg
---
+++ src/program.py
@@ -0,0 +1,4 @@
+from datetime import date
+date.today()
+print(date.today())
+print("Today's date is: " + str(date.today()))
